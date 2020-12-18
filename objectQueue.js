9665bfe8533b7b9b69161ddaee6213d7e148fd38
Marcelo Cristiano <marceloabk@hotmail.com>
2020-12-18 01:44:26 -0300

    feat: create a Queue data structure based on objects

---
+++ objectQueue.js
@@ -0,0 +1,44 @@
+export class Queue {
+  constructor() {
+    this.count = 0
+    this.lowestCount = 0
+    this.items = {}
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element
+    this.count++
+  }
+
+  dequeue() {
+    if (this.isEmpty()) return
+
+    const value = this.items[this.lowestCount]
+    delete this.items[this.lowestCount]
+    this.lowestCount++
+
+    return value
+  }
+
+  peek() {
+    return this.items[this.lowestCount]
+  }
+
+  isEmpty() {
+    return this.count === this.lowestCount
+  }
+
+  size() {
+    return this.count - this.lowestCount
+  }
+
+  clear() {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+
+  toString() {
+    return Object.values(this.items).join(', ')
+  }
+}
