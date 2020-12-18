000fc0518dccc807203a609186bf95a67dd16cd4
Marcelo Cristiano <marceloabk@hotmail.com>
2020-12-18 01:22:14 -0300

    feat: create a Stack data structure based on arrays

---
+++ arrayStack.js
@@ -0,0 +1,36 @@
+export class Stack {
+  constructor(initialItems) {
+    this.items = []
+
+    if (initialItems)
+      this.push(initialItems)
+  }
+
+  push(elements) {
+    if (Array.isArray(elements)) {
+      this.items.push(...elements)
+    } else {
+      this.items.push(elements)
+    }
+  }
+
+  pop() {
+    return this.items.pop()
+  }
+
+  peek() {
+    return this.items[this.items.length - 1]
+  }
+
+  isEmpty() {
+    return this.items.length === 0
+  }
+
+  clear() {
+    this.items.length = 0
+  }
+
+  size() {
+    return this.items.length
+  }
+}
