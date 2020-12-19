204d556693bde2a59c4e4fb9afe7f7d9f039c5a5
Marcelo Cristiano <marceloabk@hotmail.com>
2020-12-19 02:25:30 -0300

    feat: create a Deque data structure based on objects

---
+++ objectDeque.js
@@ -0,0 +1,72 @@
+export class Deque {
+  constructor() {
+    this.count = 0
+    this.lowestCount = 0
+    this.items = {}
+  }
+
+  addFront(element) {
+    if (this.isEmpty()) this.count++
+
+
+    this.items[this.lowestCount] = element
+    this.lowestCount--
+  }
+
+  addBack(element) {
+    if (this.isEmpty()) this.lowestCount--
+
+    this.items[this.count] = element
+    this.count++
+  }
+
+  removeFront() {
+    if (this.isEmpty()) return
+
+    this.lowestCount++
+    const value = this.items[this.lowestCount]
+    delete this.items[this.lowestCount]
+
+    return value
+  }
+
+  removeBack() {
+    if (this.isEmpty()) return
+
+    this.count--
+    const value = this.items[this.count]
+    delete this.items[this.count]
+
+    return value
+  }
+
+  peekFront() {
+    return this.items[this.lowestCount + 1]
+  }
+
+  peekBack() {
+    return this.items[this.count - 1]
+  }
+
+  isEmpty() {
+    return this.lowestCount === this.count
+  }
+
+  clear() {
+    this.items = {}
+    this.count = 0
+    this.lowestCount = 0
+  }
+
+  size() {
+    const size = Math.abs(this.count) + Math.abs(this.lowestCount) - 1
+    return size < 0 ? 0 : size
+  }
+
+  toString() {
+    return Object.keys(this.items)
+      .sort((a, b) => a - b)
+      .map(i => this.items[i])
+      .join(', ')
+  }
+}
